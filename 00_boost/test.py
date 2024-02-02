69ea2609c6a14b0e30f62a47b737edd49c068f1d
kwangjuri-dev <kwna2020@gmail.com>
2024-02-02 20:14:04 +0900

    test

--- 00_boost/test.py
+++ 00_boost/test.py
@@ -1,5 +1,43 @@
-print(('James', 22) < ('happy','호수','산',1000))
-print(('A','B','C') < ('해','달','별'))
-print(('가','나','다') < ('강','날','단'))
-print(('kim','park','lee') < ('son','jung','min'))
-print((0,10,100) < (0, 1, 1000))
+import requests
+import pandas as pd
+from datetime import datetime
+
+# 상수 정의
+API_KEY = 'Yjnx2bF0Xg6w8Bvabjdd4nIoBfzSKmUeOu%2FStvnUfEg80ZTLXJlZg%2FvzQ6wSlVtG44FGUf4ctUW7CErY6L35TA%3D%3D'
+API_URL = 'https://apis.data.go.kr/1230000/BidPublicInfoService04/getBidPblancListInfoServc01'
+RESPONSE_FORMAT = 'json'
+
+# 사용자 입력 받기
+numOfRows = input("한 페이지 결과 수: ")
+pageNo = input("페이지 번호: ")
+inqryDiv = input("조회 구분 (1:등록일시, 2:입찰공고번호, 3:변경일시): ")
+inqryBgnDt = input("조회 시작 일시 (yyyymmddhhmm): ")
+inqryEndDt = input("조회 종료 일시 (yyyymmddhhmm): ")
+bidNtceNo = input("입찰공고번호 (선택 사항, 없으면 빈칸으로 두세요): ")
+
+# API 호출 파라미터 설정
+params = {
+    'serviceKey': API_KEY,
+    'numOfRows': numOfRows,
+    'pageNo': pageNo,
+    'inqryDiv': inqryDiv,
+    'inqryBgnDt': inqryBgnDt,
+    'inqryEndDt': inqryEndDt,
+    'type': RESPONSE_FORMAT
+}
+
+if bidNtceNo:
+    params['bidNtceNo'] = bidNtceNo
+
+# API 호출
+response = requests.get(API_URL, params=params)
+data = response.json()
+
+# 결과를 DataFrame으로 변환
+df = pd.DataFrame(data['response']['body']['items']['item'])
+
+# 파일 이름 생성 및 CSV 파일로 저장
+filename = f"{datetime.now().strftime('%Y%m%d%H%M%S')}_{inqryBgnDt}_{inqryEndDt}_{pageNo}.csv"
+df.to_csv(filename, index=False)
+
+print(f"파일 '{filename}'에 저장되었습니다.")
---
+++ wordcloud_streamlit_v1.py
@@ -0,0 +1,27 @@
+import streamlit as st
+from wordcloud import WordCloud
+import matplotlib.pyplot as plt
+
+# Streamlit 페이지 설정
+st.title('워드클라우드 생성기')
+
+# 파일 업로드 섹션
+uploaded_file = st.file_uploader("텍스트 파일을 업로드하세요", type="txt")
+if uploaded_file is not None:
+    # 파일 읽기
+    text_data = uploaded_file.read().decode('utf-8')
+
+    # 한글 폰트 파일 경로 지정
+    font_path = 'NanumGothic.ttf'  # 예: 'fonts/NanumGothic.ttf'
+
+    # WordCloud 객체 생성
+    wordcloud = WordCloud(font_path=font_path, width=800, height=400, background_color='white').generate(text_data)
+
+    # 워드클라우드 그리기
+    plt.figure(figsize=(10, 5))
+    plt.imshow(wordcloud, interpolation='bilinear')
+    plt.axis('off')
+    plt.show()
+
+    # Streamlit에 이미지 표시
+    st.pyplot(plt)
